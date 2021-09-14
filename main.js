d753dca56899719c89269a788579e1a0b0509fd9
TarunN05 <78839555+TarunN05@users.noreply.github.com>
2021-09-14 13:12:15 +0530

    updated the js

--- main.js
+++ main.js
@@ -7,3 +7,68 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("nav-active");
 //   burger.classList.toggle("toogle");
 });
+
+
+
+ function sendmail(){
+    
+			var name = $('#name').val();
+			var email = $('#email').val();
+			var phone = $('#phone').val();
+      var message = $('#message').val();
+
+			// var body = $('#body').val();
+
+			var Body='Name: '+name+'<br>Email: '+email+'<br>Phone: '+phone+'<br>Message: '+message;
+			//console.log(name, phone, email, message);
+
+			Email.send({
+        SecureToken:"fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
+				To: 'adsvally@gmail.com',
+				From: "10xgrowthbusiness@gmail.com",
+				Subject: "New message on contact from "+name,
+				Body: Body
+			}).then(
+				message =>{
+
+					// successfulSent()
+
+
+	        if(message=='OK'){
+                mailColor.classList.toggle("success");
+				mailColor.innerHTML = "Mail Sent successful";
+			}
+			if(message != 'OK') {
+				mailColor.classList.toggle('error');
+				mailColor.innerHTML = "Mail can't Sent";
+			}
+
+					// console.log (message);
+					// if(message=='OK'){
+					// alert('Your mail has been send. Thank you for connecting.');
+					// }
+					// else{
+					// 	console.error (message);
+					// 	alert('There is error at sending message. ')
+						
+					// }
+
+				}
+			);
+
+
+
+		}
+
+
+
+const submitBtn = document.querySelector('.submit-btn');
+const inputs = document.querySelectorAll(".input");
+
+
+submitBtn.addEventListener("click" ,()=>{
+	setTimeout(()=>{
+
+		inputs.forEach(input => input.value = '');
+	}, 2000);
+});
